0044225d6aa1a41d41b3faae4a30c69ee5ea8132
Bhargav <bhargavlende11@gmail.com>
2025-11-29 00:27:30 +0530

    commit 8 : Enrolled Stu

---
+++ src/main/java/com/rcoem/sms/application/dto/EnrollmentDetails.java
@@ -0,0 +1,25 @@
+package com.rcoem.sms.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnrollmentDetails {
+    private String enrollmentId;
+    private String studentId;
+    private String studentName;
+    private String studentEmail;
+    private String department;
+    private String status;
+    private LocalDateTime enrolledAt;
+}
+
