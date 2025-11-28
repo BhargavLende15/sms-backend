e197ebf6c83f732b3449b1d3f6221a174fb6dc86
Bhargav <bhargavlende11@gmail.com>
2025-11-28 14:21:17 +0530

    Initial commit

---
+++ src/main/java/com/rcoem/sms/application/services/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.rcoem.sms.application.services;
+
+import com.rcoem.sms.application.dto.StudentDetails;
+import com.rcoem.sms.application.dto.UserDetails;
+import com.rcoem.sms.application.exceptions.InvalidCredentialsException;
+import com.rcoem.sms.application.exceptions.UserNotFoundException;
+import com.rcoem.sms.application.mapper.StudentMapper;
+import com.rcoem.sms.application.mapper.UserMapper;
+import com.rcoem.sms.domain.entities.StudentInfo;
+import com.rcoem.sms.domain.entities.UserInfo;
+import com.rcoem.sms.domain.repositories.StudentRepository;
+import com.rcoem.sms.domain.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.Objects.nonNull;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserMapper userMapper;
+
+@Autowired
+StudentService studentService;
+
+
+    @Override
+    public UserDetails registerUser(UserDetails userDetails) {
+        String uid = "USER" + UUID.randomUUID();
+        userDetails.setId(uid);
+        if(userDetails.getType()==null || userDetails.getType().isBlank()){
+            userDetails.setType("student");
+        }
+        UserDetails userDetails1= userMapper.toDto(userRepository.save(userMapper.toEntity(userDetails)));
+        if(userDetails.getType().equalsIgnoreCase("student")) {
+            studentService.createStudent(StudentDetails.builder()
+                    .id(userDetails.getId())
+                    .name(userDetails.getName())
+                    .email(userDetails.getEmail())
+                    .gender(userDetails.getGender())
+                    .points(0)
+                    .build());
+        }
+        return userDetails1;
+    }
+
+    @Override
+    public UserDetails signInUser(String email,String password) {
+        UserInfo userDetails=userRepository.findByEmail(email)
+                .orElseThrow(()-> new UserNotFoundException("User not found with email "+email));
+        if(nonNull(userDetails)){
+            if(userDetails.getPassword().equals(password)){
+                return userMapper.toDto(userDetails);
+            }
+            throw new InvalidCredentialsException("Invalid credentials");
+        }
+        throw new InvalidCredentialsException("Invalid credentials");
+    }
+
+    @Override
+    public UserDetails getUserDetails(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    public void updateUserType(UserDetails userDetails) {
+
+    }
+}
---
+++ src/main/java/com/rcoem/sms/intefaces/UserController.java
@@ -0,0 +1,41 @@
+package com.rcoem.sms.intefaces;
+
+import com.rcoem.sms.application.dto.LoginRequest;
+import com.rcoem.sms.application.dto.UserDetails;
+import com.rcoem.sms.application.exceptions.InvalidCredentialsException;
+import com.rcoem.sms.application.exceptions.UserNotFoundException;
+import com.rcoem.sms.application.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDetails> addUser(@RequestBody UserDetails userDetails) {
+        UserDetails insertedStudentDetails=userService.registerUser(userDetails);
+        return ResponseEntity.created(URI.create("/users/"+insertedStudentDetails.getId()))
+                .body(insertedStudentDetails);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> signInUser(@RequestBody LoginRequest loginRequest) {
+        try {
+            UserDetails studentDetails= userService.signInUser(loginRequest.getEmail(),loginRequest.getPassword());
+            return ResponseEntity.ok(studentDetails);
+        } catch (UserNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        } catch (InvalidCredentialsException ex){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+        }
+    }
+
+}
